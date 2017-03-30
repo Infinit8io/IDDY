3743f7f3e469dec28e975bfdff53910e3e257eb8
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-03-30 15:20:12 +0200

    New challenge form

--- IDDY/src/java/src/controllers/UserController.java
+++ IDDY/src/java/src/controllers/UserController.java
@@ -5,8 +5,13 @@
  */
 package src.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import src.entities.Users;
+import src.facades.UsersFacade;
 
 /**
  *
@@ -15,11 +20,22 @@ import javax.enterprise.context.Dependent;
 @Named(value = "userController")
 @Dependent
 public class UserController {
+    
+    @EJB
+    private UsersFacade usersFacade;
 
     /**
      * Creates a new instance of UserController
      */
     public UserController() {
+    }
+    
+    public List<String> getAllUsers(){
+        ArrayList<String> tmpList = new ArrayList<>();
+        for(Users u : usersFacade.findAll()){
+            tmpList.add(u.getPseudo());
+        }
+        return tmpList;
     }
     
 }
